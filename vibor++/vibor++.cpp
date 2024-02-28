b78eee44a347e47c2d2c07c30880dba1352fe1c9
Drago <isip_a.a.zinchenko@mpt.ru>
2024-02-28 21:28:12 +0300

    iy

---
+++ vibor++/vibor++.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string.h>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int v;
+    string kek;
+    string vowels = "aeiouyAEIOUY";
+    cout << "write a word\n";
+    cin >> kek;
+    cout << "what do you wont to do with a word\n";
+    cout << "1 reverse it\n";
+    cout << "2 delete all vowels from it\n";
+    cout << "3 delete all Consonants from it\n";
+    cout << "4 randomize all letters in it\n";
+    cin >> v;
+    if (v == 1)
+    {
+        reverse(kek.begin(), kek.end());
+    }
+    if (v == 2)
+    {
+
+    }
+    if (v == 3)
+    {
+
+    }
+    if (v == 4)
+    {
+        random_shuffle(kek.begin(), kek.end());
+    }
+    cout << kek;
+}
